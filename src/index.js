0466ab8b569b712db06e5c63b476df2df0cf8bc6
Cathy Y <cathy.yf.yang@gmail.com>
2021-11-03 08:42:57 +1000

    add tf files

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ exports.handler = async (event) => {
     templateType,
     templateS3Bucket,
     templateS3Key,
-  } = JSON.parse(event.Records[0].body);
+  } = event.Records[0].body;
 
   const emailTemplateString = await templateHelper.getTemplateFromS3Bucket(
     templateS3Bucket,
