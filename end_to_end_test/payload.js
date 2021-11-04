9b344b62aaa95f46a86f33b7d1dc2f84096b01a1
yuanningd <yuanningd@gmail.com>
2021-11-04 19:42:27 +1100

    AW-204: Refactor mock data

--- end_to_end_test/payload.js
+++ end_to_end_test/payload.js
@@ -1,16 +1,17 @@
-{
+
+exports.default = {
   "Records": [
     {
       "messageId": "19dd0b57-b21e-4ac1-bd88-01bbb068cb78",
       "receiptHandle": "MessageReceiptHandle",
-      "body": {
-        "templateType":"Verification",
+      "body": `{
+        "templateType": "Verification",
         "templateS3Bucket": "aw-email-template",
         "templateS3Key": "verification_email_template.txt",
         "userName": "John Doe",
         "email": "johndoe@test.com",
         "verificationLink": "https://somecode.com"
-      },
+      }`,
       "attributes": {
         "ApproximateFirstReceiveTimestamp": "1523232000001"
       },
@@ -22,4 +23,4 @@
       "awsRegion": "ap-southeast-2"
     }
   ]
-}
+};
