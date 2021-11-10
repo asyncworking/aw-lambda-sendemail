4ec8bc2002fbebc7b50d41aaec14bfbff72c99ed
Cathy Y <cathy.yf.yang@gmail.com>
2021-11-10 22:27:55 +1000

    update s3.tf

--- s3.tf
+++ s3.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "b6" {
 resource "aws_s3_bucket_object" "object6" {
   bucket = "jk-tf-s3-test6"
   key    = "lambda-function/lambda-jk-tf-test6.zip"
-  source = "../lambda-jk-tf-test6.zip" # its mean it depended on zip
+  source = "../var/jenkins_home/workspace/pipeline_jk_lambda_tf/lambda-jk-tf-test6.zip" # its mean it depended on zip
   depends_on = [
     aws_s3_bucket.b6,
   ]
