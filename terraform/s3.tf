cab93e6bbdf49e34699d3b0bb743bfc6cb7872a8
Cathy Y <cathy.yf.yang@gmail.com>
2021-11-10 20:39:07 +1000

    add tf files

---
+++ terraform/s3.tf
@@ -0,0 +1,23 @@
+# Create S3 bucket
+resource "aws_s3_bucket" "b6" {
+   bucket = "jk-tf-s3-test6"
+   acl = "private"
+   versioning {
+      enabled = true
+   }
+   tags = {
+     Name = "jk-tf-lambda"
+     Environment = "Uat"
+   }
+}
+
+
+# Upload zip to s3
+resource "aws_s3_bucket_object" "object1" {
+  bucket = "jk-tf-s3-test6"
+  key    = "lambda-function/lambda-jk-tf-test.zip"
+  source = "../lambda-jk-tf-test.zip" # its mean it depended on zip
+  depends_on = [
+    aws_s3_bucket.b6,
+  ]
+}
