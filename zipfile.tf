12742f9810a6b44ef45ba524656512b45877db9a
Cathy Y <cathy.yf.yang@gmail.com>
2021-11-11 17:02:55 +1000

    zipfile.tf

--- zipfile.tf
+++ zipfile.tf
@@ -1,9 +1,10 @@
-# Zip the Lamda function on the fly
-# data "archive_file" "source6" {
-#   type        = "zip"
-#   source  = "src/index.js"
-#   source  = "node_modules/dotenv"
-#   source  = "src/helpers/templateHelper.js"
-#   source  = "src/helpers/emailHelper.js"
-#   output_path = "../lambda-function/lambda-jk-tf-test6.zip"
-# }
+Zip the Lamda function on the fly
+data "archive_file" "source6" {
+  type        = "zip"
+  source_dir = "lambda-jk-tf-test6.zip"
+  source_file  = "src/index.js"
+  source_file  = "node_modules/dotenv"
+  source_file  = "src/helpers/templateHelper.js"
+  source_file  = "src/helpers/emailHelper.js"
+  output_path = "../lambda-function/lambda-jk-tf-test6.zip"
+}
