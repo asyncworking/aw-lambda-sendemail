1a74e0e1f68b746d2124f623f6af44dbc45ec96f
Cathy Y <cathy.yf.yang@gmail.com>
2021-11-12 20:20:47 +1000

    update sqs.tf

--- sqs.tf
+++ sqs.tf
@@ -19,7 +19,10 @@ resource "aws_sqs_queue" "cathy_first_sqs6" {
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
-  redrive_policy            = "{\"deadLetterTargetArn\":\"${aws_sqs_queue.terraform_queue_deadletter.arn}\",\"maxReceiveCount\":4}"
+   redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.terraform_queue_deadletter.arn
+    maxReceiveCount     = 4
+  })
 
   tags {
     Environment = "uat"
