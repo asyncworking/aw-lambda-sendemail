92a66dce9cf0d097a8ec7aaa314fa7b00300ff90
cathyyfyang <51479638+cathyyfyang@users.noreply.github.com>
2021-11-14 19:54:32 +1000

    Update setup-localtest.sh

--- end_to_end_test/setup-localtest.sh
+++ end_to_end_test/setup-localtest.sh
@@ -53,10 +53,10 @@ awslocal lambda update-function-configuration --function-name lambdaSendEmail --
         accessKeyId=test,
         secretAccessKey=test,
         sourceEmail=info@asyncworking.com,
-        sqsQueueUrl=https://0.0.0.0:4566/000000000000/AWRECEIVEQ,
-        ses_Endpoint=https://0.0.0.0:9001,
-        s3_Endpoint=https://0.0.0.0:4566,
-        sqs_Endpoint=https://0.0.0.0:4566,
+        sqsQueueUrl=http://127.0.0.1:4566/000000000000/AWRECEIVEQ,
+        ses_Endpoint=http://127.0.0.1:4566:9001,
+        s3_Endpoint=http://127.0.0.1:4566:4566,
+        sqs_Endpoint=http://127.0.0.1:4566:4566,
         }"
 
 # awslocal lambda update-function-code --function-name lambdaSendEmail --code S3Bucket="__local__",S3Key="$(pwd)"
