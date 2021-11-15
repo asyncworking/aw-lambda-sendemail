05c8507418c184c86abbf3177614d50a123c37db
cathyyfyang <51479638+cathyyfyang@users.noreply.github.com>
2021-11-15 16:02:26 +1000

    Update lambda_sqs.tf

--- lambda_sqs.tf
+++ lambda_sqs.tf
@@ -1,11 +1,11 @@
-resource "aws_lambda_event_source_mapping" "example" { 
-event_source_arn = "${aws_sqs_queue.cathy_first_sqs6.arn}" 
-enabled          = true
-function_name = "${aws_lambda_function.example6.arn}" 
-depends_on = [
-    aws_sqs_queue.cathy_first_sqs6,
-    aws_lambda_function.example6,
-    aws_sqs_queue_policy.my_sqs_policy6,
-  ]
-} 
+# resource "aws_lambda_event_source_mapping" "example" { 
+# event_source_arn = "${aws_sqs_queue.cathy_first_sqs6.arn}" 
+# enabled          = true
+# function_name = "${aws_lambda_function.example6.arn}" 
+# depends_on = [
+#     aws_sqs_queue.cathy_first_sqs6,
+#     aws_lambda_function.example6,
+#     aws_sqs_queue_policy.my_sqs_policy6,
+#   ]
+# } 
 
