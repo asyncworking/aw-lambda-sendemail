2cb459194f013be865436c8fd56f62a571a7d4b7
Sherry Xie <sherry.sy.xie@gmail.com>
2021-11-20 23:32:28 +1100

    AW-213: update template's image

--- src/test/templateHelper.test.js
+++ src/test/templateHelper.test.js
@@ -4,7 +4,7 @@ const templateHelper = require( '../helpers/templateHelper.js');
 describe('getTemplateHelper Function', () => {
     it("function should return template string stored in s3 mocked in localstack", async () => {
         const templateBucket = "aw-email-template";
-        const templateKey = "verification_email_template.txt";
+        const templateKey = "verification_email_template_updated.html";
         const response = await templateHelper.getTemplateFromS3Bucket(templateBucket, templateKey, AWS);
         expect(response).toBeTruthy();
     });
